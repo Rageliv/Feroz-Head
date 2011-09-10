69b5143d3ab0a8484a9014684acfa764b3232d88
Rageliv <rageliv@gmail.com>
2011-09-10 00:31:48 -0500

    Cleaned up code.

--- Enemy.cs
+++ Enemy.cs
@@ -11,32 +11,38 @@ public class Enemy
 {
     Vector2 myPosition;
     Boolean off = false;
-	public Enemy( float X, float Y)
-	{
+    
+    public Enemy( float X, float Y)
+    {
         myPosition.X = X;
         myPosition.Y = Y;
-
-	}
+    }
+    
     public void Off()
     {
         off = true;
     }
+    
     public Boolean isOff()
     {
         return off;
     }
+    
     public void UpdateEnemyPosition()
     {
         myPosition.Y += 1;
     }
+    
     public Vector2 GetEnemyPosition()
     {
         return myPosition;
     }
+    
     public float GetEnemyPositionY()
     {
         return myPosition.Y;
     }
+    
     public float GetEnemyPositionX()
     {
         return myPosition.X;
