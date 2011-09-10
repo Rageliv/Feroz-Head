a6a3562046bd0b8fe59da158ab333508b5e37c31
Rageliv <rageliv@gmail.com>
2011-09-10 00:32:39 -0500

    Removed pointless comment.

--- Program.cs
+++ Program.cs
@@ -5,9 +5,6 @@ namespace FerozHead
 #if WINDOWS || XBOX
     static class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
         static void Main(string[] args)
         {
             using (FerozHead game = new FerozHead())
