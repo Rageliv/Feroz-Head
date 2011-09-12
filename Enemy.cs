0002481e1343fd351b24bc7fd967fb58757a2d92
Rageliv <rageliv@gmail.com>
2011-09-12 13:01:08 -0500

    Updated Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Audio;
 using Microsoft.Xna.Framework.Content;
@@ -11,38 +11,32 @@ public class Enemy
 {
     Vector2 myPosition;
     Boolean off = false;
-    
     public Enemy( float X, float Y)
-    {
+	{
         myPosition.X = X;
         myPosition.Y = Y;
-    }
-    
+
+	}
     public void Off()
     {
         off = true;
     }
-    
     public Boolean isOff()
     {
         return off;
     }
-    
     public void UpdateEnemyPosition()
     {
         myPosition.Y += 1;
     }
-    
     public Vector2 GetEnemyPosition()
     {
         return myPosition;
     }
-    
     public float GetEnemyPositionY()
     {
         return myPosition.Y;
     }
-    
     public float GetEnemyPositionX()
     {
         return myPosition.X;
